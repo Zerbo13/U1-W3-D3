1dd3aa1bfe0b3a609b8df65b11fb5b4e2343b942
Mattia Zerbini <zermatty6@gmail.com>
2025-10-08 16:42:11 +0200

    Add new file JS, basic file HTML and CSS

---
+++ script/D8.js
@@ -0,0 +1,42 @@
+const form = document.getElementById('task-form')
+
+const deleteTask = function(e){
+    console.log('Elimino la task', e)
+
+    e.target.parentElement.remove()
+}
+
+const handleForm = function(e){
+    e.preventDefault()
+    console.log('Recuperiamo i dati')
+
+    //1) recuperiamo i riferimenti a tutti gli input
+    const taskInput = document.getElementById('task')
+
+    //2) recuperiamo i valori
+    const valoreTask = taskInput.value 
+
+    //3) conferma con il console.log
+    console.log(valoreTask)
+
+    //4) Task salvate possiamo fare quello che vogliamo
+    //vado a creare un div
+    const list = document.createElement('div')
+    list.classList.add('task-list')
+
+    //riempio il div con i dati presi dal form
+    list.innerHTML = `
+    <p>${valoreTask}</p>
+    <button onclick = "deleteTask(event)">Cancella</button>
+    `
+    //la list è completa ma ancora non esiste, adesso la vado a creare
+
+    const container = document.getElementById('save-task')
+    container.appendChild(list)
+
+    //reset dei campi
+    form.reset()
+
+}
+
+form.addEventListener('submit', handleForm)
